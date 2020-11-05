ef47bec1b7df39c9a5375e17ca17d2a984662f28
rdb112 <drrdb112@gmail.com>
2020-11-06 11:00:45 +1300

    Changed 3 initial variables to static final constants.

--- Module3_Pong1/src/Pong.java
+++ Module3_Pong1/src/Pong.java
@@ -2,8 +2,22 @@ import javax.swing.JFrame;
 
 public class Pong extends JFrame {
 //This is the main class for the game
+
+	static final int WIDTH = 600;
+	static final int HEIGHT = 600;
+	static final String WINDOW_TITLE = "Pong Game V1";
+	
+	public Pong() {
+		setTitle(WINDOW_TITLE);
+		setSize(HEIGHT, WIDTH);
+		setResizable(false);
+		setVisible(true);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+	}
+
 	public static void main(String[] args) {
-		
+
+		new Pong();
 	}
 
 }
