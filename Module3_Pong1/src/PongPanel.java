4685e1f45d4982bc75c754a21637619cb14f1496
rdb112 <drrdb112@gmail.com>
2020-11-06 11:33:01 +1300

    Added starting content for PongPanel class.

--- Module3_Pong1/src/PongPanel.java
+++ Module3_Pong1/src/PongPanel.java
@@ -1,4 +1,38 @@
+import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import javax.swing.JPanel;
 
-public class PongPanel {
+public class PongPanel extends JPanel implements ActionListener, KeyListener  {
+
+	public PongPanel() {
+		setBackground(Color.BLACK);
+	}
+	
+	@Override
+	public void keyTyped(KeyEvent event) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyPressed(KeyEvent event) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyReleased(KeyEvent event) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent event) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
