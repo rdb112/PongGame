f7410e8f74cdb94ed51121175bd84a18e1f9c649
rdb112 <drrdb112@gmail.com>
2020-11-09 10:29:09 +1300

    Changes made from page adding dotted line

--- Module3_Pong1/src/PongPanel.java
+++ Module3_Pong1/src/PongPanel.java
@@ -1,10 +1,12 @@
+import java.awt.BasicStroke;
 import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Stroke;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
-import java.awt.Graphics;
-import java.awt.Graphics2D;
 
 import javax.swing.JPanel;
 import javax.swing.Timer;
@@ -42,6 +44,7 @@ public class PongPanel extends JPanel implements ActionListener, KeyListener  {
 	@Override
 	public void actionPerformed(ActionEvent event) {
 		update();
+		repaint();
 		
 	}
 	private void update() {
@@ -51,8 +54,18 @@ public class PongPanel extends JPanel implements ActionListener, KeyListener  {
 	@Override
 	public void paintComponent(Graphics g) {
 	     super.paintComponent(g);
+	     paintDottedLine(g);
 	     //g.setColor(Color.WHITE);
 	     //g.fillRect(20, 20, 100, 100);
+	 }
+	
+	private void paintDottedLine(Graphics g) {
+	      Graphics2D g2d = (Graphics2D) g.create();
+	         Stroke dashed = new BasicStroke(3, BasicStroke.CAP_BUTT, BasicStroke.JOIN_BEVEL, 0, new float[]{9}, 0);
+	         g2d.setStroke(dashed);
+	         g2d.setPaint(Color.WHITE);
+	         g2d.drawLine(getWidth() / 2, 0, getWidth() / 2, getHeight());
+	         g2d.dispose();
 	 }
 
 }
