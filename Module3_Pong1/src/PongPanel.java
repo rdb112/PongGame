71b9d14ff6919832b5b3d5ea728ec7732287b21d
rdb112 <drrdb112@gmail.com>
2020-11-09 10:23:20 +1300

    Added several methods to pong class, white rectangle on screen test.
    From page  Painting to the screen.

--- Module3_Pong1/src/PongPanel.java
+++ Module3_Pong1/src/PongPanel.java
@@ -3,14 +3,22 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+
 import javax.swing.JPanel;
+import javax.swing.Timer;
 
 public class PongPanel extends JPanel implements ActionListener, KeyListener  {
 
 	private final static Color BGCOLOR = Color.BLACK;
+	private final static int TIMER_DELAY = 5;
+	
 	
 	public PongPanel() {
 		setBackground(BGCOLOR);
+		Timer timer = new Timer(TIMER_DELAY, this);
+        timer.start();
 	}
 	
 	@Override
@@ -33,8 +41,18 @@ public class PongPanel extends JPanel implements ActionListener, KeyListener  {
 
 	@Override
 	public void actionPerformed(ActionEvent event) {
+		update();
+		
+	}
+	private void update() {
 		// TODO Auto-generated method stub
 		
 	}
+	@Override
+	public void paintComponent(Graphics g) {
+	     super.paintComponent(g);
+	     //g.setColor(Color.WHITE);
+	     //g.fillRect(20, 20, 100, 100);
+	 }
 
 }
