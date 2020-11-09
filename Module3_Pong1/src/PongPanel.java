e50a975a9cb2011e5ed2486416962afb6018d198
rdb112 <drrdb112@gmail.com>
2020-11-10 10:57:01 +1300

    End of stage 3 of module 3

--- Module3_Pong1/src/PongPanel.java
+++ Module3_Pong1/src/PongPanel.java
@@ -15,24 +15,27 @@ public class PongPanel extends JPanel implements ActionListener, KeyListener {
 
 	private final static Color BGCOLOR = Color.BLACK;
 	private final static int TIMER_DELAY = 5;
+	private final static int BALL_MOVEMENT_SPEED = 2;
 
 	Ball ball;
 	// boolean gameInitialised = false;
 	GameState gameState = GameState.Initialising;
-	
-	 Paddle paddle1, paddle2;
+
+	Paddle paddle1, paddle2;
 
 	public PongPanel() {
 		setBackground(BGCOLOR);
 		Timer timer = new Timer(TIMER_DELAY, this);
 		timer.start();
+		addKeyListener(this);
+		setFocusable(true);
 	}
 
 	public void createObjects() {
 		ball = new Ball(getWidth(), getHeight());
 		paddle1 = new Paddle(Player.One, getWidth(), getHeight());
-		paddle2 = new Paddle(Player.Two, getWidth(), getHeight());	
-		
+		paddle2 = new Paddle(Player.Two, getWidth(), getHeight());
+
 	}
 
 	@Override
@@ -43,13 +46,26 @@ public class PongPanel extends JPanel implements ActionListener, KeyListener {
 
 	@Override
 	public void keyPressed(KeyEvent event) {
-		// TODO Auto-generated method stub
-
+		if (event.getKeyCode() == KeyEvent.VK_UP) {
+			paddle2.setyVelocity(-1);
+		} else if (event.getKeyCode() == KeyEvent.VK_DOWN) {
+			paddle2.setyVelocity(1);
+		}
+		if (event.getKeyCode() == KeyEvent.VK_W) {
+			paddle1.setyVelocity(-1);
+		} else if (event.getKeyCode() == KeyEvent.VK_S) {
+			paddle1.setyVelocity(1);
+		}
 	}
 
 	@Override
 	public void keyReleased(KeyEvent event) {
-		// TODO Auto-generated method stub
+		if (event.getKeyCode() == KeyEvent.VK_UP || event.getKeyCode() == KeyEvent.VK_DOWN) {
+			paddle2.setyVelocity(0);
+		}
+		if (event.getKeyCode() == KeyEvent.VK_W || event.getKeyCode() == KeyEvent.VK_S) {
+			paddle1.setyVelocity(0);
+		}
 
 	}
 
@@ -60,20 +76,51 @@ public class PongPanel extends JPanel implements ActionListener, KeyListener {
 
 	}
 
+	public void moveObject(Sprite sprite) {
+		sprite.setXPosition(sprite.getXPosition() + sprite.getxVelocity(), getWidth());
+		sprite.setXPosition(sprite.getYPosition() + sprite.getyVelocity(), getHeight());
+	}
+
+	public void checkWallBounce() {
+		if (ball.getXPosition() <= 0) {
+			ball.setxVelocity(-ball.getxVelocity());
+			resetBall();
+		} else if (ball.getXPosition() >= getWidth() - ball.getWidth()) {
+			ball.setxVelocity(-ball.getxVelocity());
+			resetBall();
+		}
+		if (ball.getYPosition() <= 0 || ball.getYPosition() >= getHeight() - ball.getHeight()) {
+			// Hit top or bottom of screen
+			ball.setyVelocity(-ball.getyVelocity());
+		}
+	}
+
+	public void resetBall() {
+		if (ball.getXPosition() <= 0) {
+			ball.resetToInitialPosition();
+		}
+	}
+
 	private void update() {
 		switch (gameState) {
 		case Initialising: {
 			createObjects();
 			gameState = GameState.Playing;
+			ball.setxVelocity(BALL_MOVEMENT_SPEED);
+			ball.setyVelocity(BALL_MOVEMENT_SPEED);
 			break;
 		}
 		case Playing: {
+			moveObject(paddle1);
+			moveObject(paddle2);
+			moveObject(ball);
+			checkWallBounce();
 			break;
 		}
 		case GameOver: {
 			break;
 		}
-	}
+		}
 
 	}
 
